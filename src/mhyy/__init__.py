753b115b99996d8502d4f74f6c0c40cca0a3b02a
GuangChen2333 <guangchenworks@outlook.com>
2023-10-03 22:22:03 +0800

    release: v1.0.2

--- src/mhyy/__init__.py
+++ src/mhyy/__init__.py
@@ -1,3 +1,7 @@
+# Entities
 from ._client import Client
 from ._user import User
+
+# Enums
+from ._user import UserType, UserChannel, UserCGGameBiz, UserOpBiz, UserCps
 from ._notification import NotificationType, NotificationStatus
