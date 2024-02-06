fb71ff89ecebf7408307cbf1207c9346d576b14d
GuangChen2333 <guangchenworks@outlook.com>
2024-02-06 11:31:25 +0800

    doc: notification doc

--- src/mhyy/_notification.py
+++ src/mhyy/_notification.py
@@ -6,6 +6,9 @@ T = TypeVar("T", bound="Notification")
 
 
 class Notification:
+    """
+    通知类
+    """
     def __init__(
             self,
             id_: str,
@@ -28,6 +31,15 @@ class Notification:
 
     @classmethod
     def from_data_dict(cls, data: dict) -> T:
+        """
+        从特定的数据结构生成 Notification。
+
+        Args:
+            data: 消息数据。
+
+        Returns:
+            包装后的消息数据。
+        """
         return cls(
             id_=data['id'],
             status=NotificationStatus.get_status_by_name(data['status']),
@@ -40,33 +52,81 @@ class Notification:
         )
 
     @property
-    def id(self):
+    def id(self) -> str:
+        """
+        通知 ID。
+
+        Returns:
+            该通知的 ID。
+        """
         return self._id
 
     @property
-    def status(self):
+    def status(self) -> NotificationStatus:
+        """
+        通知状态。
+
+        Returns:
+            该通知的通知状态。
+        """
         return self._status
 
     @property
-    def type(self):
+    def type(self) -> NotificationType:
+        """
+        通知种类。
+
+        Returns:
+            该通知的通知种类。
+        """
         return self._type
 
     @property
-    def priority(self):
+    def priority(self) -> int:
+        """
+        作用未知，根据名称推测是通知的优先级。
+
+        Returns:
+            该通知的优先级。
+        """
         return self._priority
 
     @property
-    def source(self):
+    def source(self) -> str:
+        """
+        作用未知。
+
+        Returns:
+            一个字符串，作用未知。
+        """
         return self._source
 
     @property
-    def desc(self):
+    def desc(self) -> str:
+        """
+        作用未知，根据名称推测是通知的描述。
+
+        Returns:
+            该通知的描述。
+        """
         return self._desc
 
     @property
-    def msg(self):
+    def msg(self) -> str:
+        """
+        通知内容。
+
+        Returns:
+            一个字符串，包含了 json 文本格式的该通知的内容。
+        """
         return self._msg
 
     @property
     def create_at(self):
+        """
+        通知创建时间。
+
+        Returns:
+            一个字符串，是秒级的时间戳 (10位)，描述了该通知何时被创建。
+        """
         return self._created_at
